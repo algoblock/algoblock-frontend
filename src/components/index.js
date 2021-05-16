3080b00c8dbcc4cbf6127e5c56494eddd5c87597
bretsky <bretskyselby99@gmail.com>
2021-05-15 22:46:16 -0700

    Remove references to deleted component, add gh-pages

--- src/components/index.js
+++ src/components/index.js
@@ -1,37 +1,35 @@
 import BigButton from './BigButton/BigButton';
 import Button from './Button/Button';
+import Card from './Card/Card';
+import Column from './Column/Column';
+import Divider from './Divider/Divider';
 import Header from './Header/Header';
 import HeaderLink from './HeaderLink/HeaderLink';
+import InvertedButton from './InvertedButton/InvertedButton';
+import LandingPage from './LandingPage/LandingPage';
 import Link from './Link/Link';
 import LinkBar from './LinkBar/LinkBar';
 import Logo from './Logo/Logo';
+import Row from './Row/Row';
+import ScrollingDivider from './ScrollingDivider/ScrollingDivider';
 import SignInLink from './SignInLink/SignInLink';
 import Splash from './Splash/Splash';
-import BackgroundGraph from './BackgroundGraph/BackgroundGraph';
-import Divider from './Divider/Divider';
-import ScrollingDivider from './ScrollingDivider/ScrollingDivider';
-import Row from './Row/Row';
-import Column from './Column/Column';
-import LandingPage from './LandingPage/LandingPage';
-import Card from './Card/Card';
-import InvertedButton from './InvertedButton/InvertedButton';
 
 export {
-	BackgroundGraph,
-	Divider,
 	BigButton,
 	Button,
+	Card,
+	Column,
+	Divider,
 	Header,
 	HeaderLink,
+	InvertedButton,
+	LandingPage,
 	Link,
 	LinkBar,
 	Logo,
+	Row,
+	ScrollingDivider,
 	SignInLink,
 	Splash,
-	ScrollingDivider,
-	Row,
-	Column,
-	LandingPage,
-	Card,
-	InvertedButton,
 }
