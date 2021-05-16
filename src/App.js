aada9798d1bd077dd0eaa200614497e469282773
bretsky <bretskyselby99@gmail.com>
2021-05-16 01:40:45 -0700

    Remove helmet, update site title and description in index.html

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import logo from './logo.svg';
 import {LandingPage} from './components';
-import {Helmet} from 'react-helmet';
 import './App.css';
-
-const title = "Algoblock: Algorithmic trading made easy";
-const description = "Algoblock is the easy way to automate your trading and make your investing smarter. Build your own trading strategy today!"
 
 function App() {
   return (
     <div className="App">
-      <Helmet>
-        <title>{title}</title>
-        <meta name="description" content={description}/>
-      </Helmet>
       <LandingPage/>
     </div>
   );
