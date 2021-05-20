44aef690918abf29f2fe5129e0d35a1860f7088d
bretsky <bretskyselby99@gmail.com>
2021-05-20 08:50:21 -0700

    Separate images into vector and raster folders

--- src/img/index.js
+++ src/img/index.js
@@ -1,17 +1,19 @@
-import AlgoblockPng from './algoblock.png';
-import AlgoblockSvg from './algoblock.svg';
-import AlgoblockNoOverlapSvg from './algoblock-no-overlap.svg';
-import BgPng from './bg.png';
-import BgSvg from './bg.svg';
-import RevenuePng from './revenue.png';
-import SplashArtPng from './splashart.png';
-import WalletPng from './wallet.png';
-import RocketPng from './rocket.png';
+import AlgoblockPng from './raster/algoblock.png';
+import AlgoblockSvg from './vector/algoblock.svg';
+import AlgoblockNoOverlapSvg from './vector/algoblock-no-overlap.svg';
+import AlgoblockOnelineSvg from './vector/algoblock-oneline.svg';
+import BgPng from './raster/bg.png';
+import BgSvg from './vector/bg.svg';
+import RevenuePng from './raster/revenue.png';
+import SplashArtPng from './raster/splashart.png';
+import WalletPng from './raster/wallet.png';
+import RocketPng from './raster/rocket.png';
 
 export {
 	AlgoblockPng,
 	AlgoblockSvg,
 	AlgoblockNoOverlapSvg,
+	AlgoblockOnelineSvg,
 	BgPng,
 	BgSvg,
 	RevenuePng,
