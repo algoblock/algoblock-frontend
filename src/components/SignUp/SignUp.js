efd97189c03332792f33c8cf57b8fe520ba1d84c
Malik0411 <43456911+Malik0411@users.noreply.github.com>
2022-02-01 22:32:17 -0500

    Implemented state sharing between components to retrieve user information from input fields on signup

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,54 +1,68 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { GoogleLogin } from 'react-google-login';
+import React, {useContext, useEffect, useState} from 'react';
 import styles from './SignUp.module.scss';
-import {SignInInput, Link, LogInButton, Button, GoogleSignInButton} from '../';
+import {SignInInput, Link, SignUpButton, GoogleSignInButton} from '../';
 import {AlgoblockOnelineSvg} from '../../img';
+import {UserContext} from "../../providers/UserProvider";
+import {Redirect} from "react-router-dom";
 
-const SignUp = () => (
-  <div className={styles.SignUp}>
-    <img src={AlgoblockOnelineSvg} width={230}/>
-    <div className={styles.Title}>Let's get started</div>
-    <div className={styles.UserInfo}>
-      <SignInInput type="first_name" placeholder="First Name"/>
-      <div style={{paddingRight: 7}}/>
-      <SignInInput type="last_name" placeholder = "Last Name"/>
+const SignUp = () => {
+  // User object containing information about authentication status
+  const user = useContext(UserContext);
+
+  // Stateful collection of user information shared between components
+  const [first, setfirst] = useState('');
+  const [last, setlast] = useState('');
+  const [email, setemail] = useState('');
+  const [pass, setpass] = useState('');
+
+  // After successful authentication
+  const [redirect, setredirect] = useState(null);
+  useEffect(() => {
+    if (user) {
+      setredirect('/dashboard')
+    }
+  }, [user])
+  if (redirect) {
+    return <Redirect to={redirect}/>
+  }
+
+  return(
+    <div className={styles.SignUp}>
+      <img src={AlgoblockOnelineSvg} width={230}/>
+      <div className={styles.Title}>Let's get started</div>
+      <div className={styles.UserInfo}>
+        <SignInInput type="first_name" placeholder="First Name" setvalue={setfirst}/>
+        <div style={{paddingRight: 7}}/>
+        <SignInInput type="last_name" placeholder="Last Name" setvalue={setlast}/>
+      </div>
+      <div className={styles.Login}>
+        <SignInInput type="email" placeholder="Email" setvalue={setemail}/>
+        <div className={styles.InputSpacer}/>
+        <SignInInput type="password" placeholder="Password" setvalue={setpass}/>
+        <div className={styles.ForgotPasswordSpacer}/>
+        <div className={styles.LogInSpacer}/>
+        <div className={styles.CenterWrapper}>
+          <SignUpButton email={email} pass={pass} first={first} last={last}>
+            Sign Up
+          </SignUpButton>
+        </div>
+        <div className={styles.OrDivider}>
+          <div className={styles.Divider}/>
+          <div className={styles.Or}>or</div>
+          <div className={styles.Divider}/>
+        </div>
+        <div className={styles.CenterWrapper}>
+          <GoogleSignInButton>Sign in with Google</GoogleSignInButton>
+        </div>
+      </div>
+      <div className={styles.CenterWrapper}>
+        <div className={styles.SignUpText}>
+          Already have an account?&nbsp;<Link to="/login"><span className={styles.SignUpLink}>Log in</span></Link>
+        </div>
+      </div>
     </div>
-    <div className={styles.Login}>
-    	<SignInInput type="email" placeholder="Email"/>
-    	<div className={styles.InputSpacer}/>
-    	<SignInInput type="password" placeholder="Password"/>
-    	<div className={styles.ForgotPasswordSpacer}/>
-	    <div className={styles.LogInSpacer}/>
-	    <div className={styles.CenterWrapper}>
-		    <LogInButton>
-		    	Sign Up
-		    </LogInButton>
-	    </div>
-	    <div className={styles.OrDivider}>
-	    	<div className={styles.Divider}/>
-	    	<div className={styles.Or}>or</div>
-	    	<div className={styles.Divider}/>
-	    </div>
-	    <div className={styles.CenterWrapper}>
-		    <GoogleLogin
-			    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-			    render={() => (<GoogleSignInButton>Sign up with Google</GoogleSignInButton>)}
-			    buttonText="Sign in with Google"
-			    disabled={false}
-			    onSuccess={() => {}}
-			    onFailure={() => {}}
-			    cookiePolicy={'single_host_origin'}
-			  />
-		</div>
-    </div>
-    <div className={styles.CenterWrapper}>
-		<div className={styles.SignUpText}>
-			Already have an account?&nbsp;<Link to="/login"><span className={styles.SignUpLink}>Log in</span></Link>
-		</div>
-	</div>
-  </div>
-);
+  );
+}
 
 SignUp.propTypes = {};
 
