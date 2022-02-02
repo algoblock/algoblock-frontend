073d5a818ec4fe5f3af670a23bf4c4181f4edf46
Malik0411 <43456911+Malik0411@users.noreply.github.com>
2022-02-01 22:30:38 -0500

    Added new functions to signup/signin with email/password and update a user displayName

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions we need from the SDKs we need
-import firebase from "firebase/compat/app"
+import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
+import {updateProfile} from "firebase/auth";
 
 // Algoblock's Firebase configuration
 const firebaseConfig = {
@@ -17,24 +18,62 @@ firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
 
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => {
-  auth.signInWithPopup(provider)
-    .then((res) => {
-      console.log(res.user)
+/*
+The following are functions that create/authenticate a user from signup/signin
+The user object firebase.UserCredential is exported after successful authentication
+This object should be used wherever the user's information is required
+*/
+
+export const createUserWithEmailPass = (email, pass, displayName) => {
+  auth.createUserWithEmailAndPassword(email, pass)
+    .then(() => {
+      updateDisplayName(displayName);
     })
     .catch((error) => {
-      console.log(error.message)
+      const errorCode = error.code;
+      if (errorCode === 'auth/weak-password') {
+        alert('The password is too weak.');
+      } else if (errorCode === 'auth/email-already-exists') {
+        alert('The provided email is already in use by an existing user.');
+      } else {
+        alert(error.message);
+      }
+    })
+}
+
+export const updateDisplayName = (displayName) => {
+  updateProfile(auth.currentUser, {
+    displayName: displayName,
+  }).catch((error) => {
+      alert(error.message);
+    })
+}
+
+export const signInWithEmailPass = (email, pass) => {
+  auth.signInWithEmailAndPassword(email, pass)
+    .catch((error) => {
+      alert(error.message);
+    })
+}
+
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => {
+  auth.signInWithPopup(googleProvider)
+    .catch((error) => {
+      alert(error.message);
     });
 }
 
+/*
+The following are used to revoke a user's authentication status (JWT)
+*/
 export const signOut = () => {
   auth.signOut()
     .then(() => {
       console.log('Logged Out')
     })
     .catch((error) => {
-      console.log(error.message)
+      alert(error.message)
     })
 }
